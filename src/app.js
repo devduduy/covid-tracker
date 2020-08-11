dcb38f8db36639a59aa8011a3deb64278674df4e
devduduy <yudhapermana166@gmail.com>
2020-08-11 20:31:41 +0700

    Make components, Cards, Chart, CountryPicker

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>App Comp</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
